27903c2df59b020fe29e3c098cf694e4e60d388d
Sana S Navas <sanasnavas10@gmail.com>
2025-05-14 20:50:50 +0530

    Update complex.java

--- complex.java
+++ complex.java
@@ -2,24 +2,24 @@ import java.util.Scanner;
 
 public class complex {
     double real;
-
     double img;
 
     complex() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter real part");
+        real = sc.nextDouble();  // no 'this' used
 
-        this.real = sc.nextDouble();
         System.out.println("Enter img part");
-        this.img = sc.nextDouble();
-
+        img = sc.nextDouble();   // no 'this' used
     }
 
     public static void main(String[] args) {
         complex c1 = new complex();
         complex c2 = new complex();
+
         double r = c1.real + c2.real;
         double i = c1.img + c2.img;
+
         System.out.println(r + "+" + i + "i");
     }
 }
